592445db3b3a281e9f7eba4d54e045203f49bab3
Noisrev <62750690+Noisrev@users.noreply.github.com>
2023-02-20 20:56:33 +0800

    修正 build.ps1 中错误的参数

--- build.ps1
+++ build.ps1
@@ -29,7 +29,7 @@ function Build-Extensions {
 function Build-MainAppx {
     $SolutionName = "GZSkinsX.sln"
     $MainAppxProject = "src\appx\GZSkinsX\GZSkinsX.csproj"
-    msbuild $MainAppxProject $MSBuildProperties /p:SelfContained=false /p:WindowsAppSDKSelfContained=false /p:GenerateTemporaryStoreCertificate=false
+    msbuild $MainAppxProject /p:Configuration=$Configuration /p:Platform=$Platform /p:SelfContained=false /p:WindowsAppSDKSelfContained=false /p:GenerateTemporaryStoreCertificate=false
     if ($LastExitCode -ne 0) {
         Write-Error "Failed to build the main appx project."
         exit 1
