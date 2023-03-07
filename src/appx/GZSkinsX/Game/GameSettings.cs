ca552cc17529e5162e968dffd8101ea167e46a85
Wenveo <Wenveo@outlook.com>
2023-03-08 01:04:25 +0800

    添加 GameSettings 中缺少的注释

--- src/appx/GZSkinsX/Game/GameSettings.cs
+++ src/appx/GZSkinsX/Game/GameSettings.cs
@@ -15,18 +15,45 @@ using GZSkinsX.Api.Settings;
 
 namespace GZSkinsX.Game;
 
+/// <summary>
+/// 表示用于存储游戏服务的基本数据配置
+/// </summary>
 [Shared, Export]
 internal sealed class GameSettings
 {
+    /// <summary>
+    /// 表示当前设置节点的 <seealso cref="Guid"/> 字符串值
+    /// </summary>
     private const string THE_GUID = "BFEEF60A-222B-422C-B459-83FC27E84290";
+
+    /// <summary>
+    /// 用于存储游戏根目录值的键字符串常量
+    /// </summary>
     private const string ROOT_DIRECTORY_NAME = "RootDirectory";
+
+    /// <summary>
+    /// 用于存储当前游戏区域的键字符串常量
+    /// </summary>
     private const string CURRENT_REGION_GUID = "CurrentRegion";
 
+    /// <summary>
+    /// 用于存储本地数据的数据节点
+    /// </summary>
     private readonly ISettingsSection _settingsSection;
 
+    /// <summary>
+    /// 表示当前游戏的根目录的字段
+    /// </summary>
     private string _rootDirectory;
+
+    /// <summary>
+    /// 表示当前游戏区域的字段
+    /// </summary>
     private GameRegion _currentRegion;
 
+    /// <summary>
+    /// 获取或设置当前游戏的根目录
+    /// </summary>
     public string RootDirectory
     {
         get => _rootDirectory;
@@ -40,6 +67,9 @@ internal sealed class GameSettings
         }
     }
 
+    /// <summary>
+    /// 获取或设置当前游戏所在的区域
+    /// </summary>
     public GameRegion CurrentRegion
     {
         get => _currentRegion;
@@ -54,9 +84,8 @@ internal sealed class GameSettings
     }
 
     /// <summary>
-    /// 
+    /// 初始化 <see cref="GameSettings"/> 的新实例
     /// </summary>
-    /// <param name="settingsService"></param>
     [ImportingConstructor]
     public GameSettings(ISettingsService settingsService)
     {
