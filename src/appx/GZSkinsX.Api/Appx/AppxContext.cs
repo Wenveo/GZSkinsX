a9fba0c81a89f79209290f0a1b4999cbcbd3cfba
Wenveo <Wenveo@outlook.com>
2023-03-25 21:15:22 +0800

    移除 AppxContext 中的 IsMSIX 和 GetCurrentPackageFullName
    * 因为 UWP 应用始终为 Package，因此不需要判断是否为非 Msix

--- src/appx/GZSkinsX.Api/Appx/AppxContext.cs
+++ src/appx/GZSkinsX.Api/Appx/AppxContext.cs
@@ -5,11 +5,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#nullable enable
-
 using System;
-using System.Runtime.InteropServices;
-using System.Text;
 
 using Windows.ApplicationModel;
 
@@ -21,38 +17,16 @@ namespace GZSkinsX.Api.Appx;
 public static class AppxContext
 {
     /// <summary>
-    /// 获取当前应用包的名称
-    /// </summary>
-    /// <param name="packageFullNameLength">用于返回字符串长度</param>
-    /// <param name="packageFullName">用于存储应用包名称</param>
-    /// <returns></returns>
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    private static extern int GetCurrentPackageFullName(
-        ref int packageFullNameLength,
-        StringBuilder? packageFullName);
-
-    /// <summary>
     /// 初始化 <see cref="AppxContext"/> 的静态成员
     /// </summary>
     static AppxContext()
     {
-        var length = 0;
-
-        IsMSIX = GetCurrentPackageFullName(ref length, null) != 15700L;
-        AppxDirectory = IsMSIX ? Package.Current.InstalledLocation.Path : Environment.CurrentDirectory;
+        AppxDirectory = Package.Current.InstalledLocation.Path;
         AppxVersion = new Version(2, 0, 0, 0);
     }
 
     /// <summary>
-    /// 获取当前应用程序是否为 MSIX 应用
-    /// </summary>
-    public static bool IsMSIX { get; }
-
-    /// <summary>
     /// 获取当前应用程序根目录
-    /// <para>
-    /// 根据 <see cref="IsMSIX"/> 的不同，返回的目录也不同
-    /// </para>
     /// </summary>
     public static string AppxDirectory { get; }
 
