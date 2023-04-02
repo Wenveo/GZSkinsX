b6cc1df6fc5f03b2549c2e9ebbc2d29f06c4a464
Wenveo <Wenveo@outlook.com>
2023-04-02 15:31:25 +0800

    删除 Navigation 项目中多余的内容，并添加 DevTools 的本地化资源

--- src/appx/GZSkinsX.Appx.Navigation/DefineNavigationGroups.cs
+++ src/appx/GZSkinsX.Appx.Navigation/DefineNavigationGroups.cs
@@ -6,12 +6,8 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 using System.Composition;
-using System.Threading.Tasks;
 
 using GZSkinsX.Api.Navigation;
-
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 
 namespace GZSkinsX.Appx.Navigation;
 
@@ -19,46 +15,10 @@ namespace GZSkinsX.Appx.Navigation;
 [NavigationGroupMetadata(Guid = NavigationConstants.NAV_MAIN_GROUP, Order = NavigationConstants.ORDER_MAIN_GROUP)]
 internal sealed class NavMainGroup : INavigationGroup { }
 
-
 [Shared, ExportNavigationGroup]
-[NavigationGroupMetadata(Header = "Dev Tools", Guid = NavigationConstants.NAV_DEV_TOOLS_GROUP, Order = NavigationConstants.ORDER_DEV_TOOLS_GROUP)]
+[NavigationGroupMetadata(Header = "resx:GZSkinsX.Appx.Navigation/Resources/NavigationGroup_DevTools_Header", Guid = NavigationConstants.NAV_DEV_TOOLS_GROUP, Order = NavigationConstants.ORDER_DEV_TOOLS_GROUP)]
 internal sealed class NavDevToolsGroup : INavigationGroup { }
 
 [Shared, ExportNavigationGroup]
 [NavigationGroupMetadata(Guid = NavigationConstants.NAV_FOOTER_GROUP, Order = NavigationConstants.ORDER_FOOTER_GROUP, Placement = NavigationItemPlacement.Footer)]
 internal sealed class NavFooterGroup : INavigationGroup { }
-
-
-[Shared, ExportNavigationItem]
-[NavigationItemMetadata(Guid = "804F13B3-CE9B-4640-ACE0-960B6FA97AA3", Header = "TestPageA", OwnerGuid = NavigationConstants.NAV_MAIN_GROUP, PageType = typeof(NavigationRootPage))]
-internal sealed class TestPageA : INavigationItem
-{
-    public IconElement Icon => new SymbolIcon(Symbol.Home);
-
-    public async Task OnNavigatedFromAsync()
-    {
-        await Task.CompletedTask;
-    }
-
-    public async Task OnNavigatedToAsync(NavigationEventArgs args)
-    {
-        await Task.CompletedTask;
-    }
-}
-
-[Shared, ExportNavigationItem]
-[NavigationItemMetadata(Guid = "536A2186-C444-4AA9-ACF4-4E6602E82765", Header = "TestPageB", OwnerGuid = NavigationConstants.NAV_MAIN_GROUP, PageType = typeof(NavigationRootPage))]
-internal sealed class TestPageB : INavigationItem
-{
-    public IconElement Icon => new SymbolIcon(Symbol.SlideShow);
-
-    public async Task OnNavigatedFromAsync()
-    {
-        await Task.CompletedTask;
-    }
-
-    public async Task OnNavigatedToAsync(NavigationEventArgs args)
-    {
-        await Task.CompletedTask;
-    }
-}
