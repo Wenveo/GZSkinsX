cf7de59b2e95f67f7481dde5213d6858f31cce22
Wenveo <Wenveo@outlook.com>
2023-04-03 23:45:45 +0800

    为 PreloadPage 添加错误处理以及信息提示

--- src/appx/GZSkinsX.Appx.Preload/PreloadPage.xaml.cs
+++ src/appx/GZSkinsX.Appx.Preload/PreloadPage.xaml.cs
@@ -6,12 +6,13 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 
 using GZSkinsX.Api.Appx;
+using GZSkinsX.Api.MRT;
 using GZSkinsX.Api.WindowManager;
 
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -24,21 +25,65 @@ namespace GZSkinsX.Appx.Preload;
 /// </summary>
 public sealed partial class PreloadPage : Page
 {
+    private readonly IMRTCoreMap _mrtCoreMap;
+
     public PreloadPage()
     {
+        var mainResourceMap = AppxContext.MRTCoreService.MainResourceMap;
+        _mrtCoreMap = mainResourceMap.GetSubtree("GZSkinsX.Appx.Preload/Resources");
+
         InitializeComponent();
+    }
+
+    private void ShowCrashMessage(string message)
+    {
+        DefaultContent.Visibility = Visibility.Collapsed;
+        CrashContent.Visibility = Visibility.Visible;
+
+        CrashTextHost.Text = message;
+    }
+
+    private async Task<bool> CheckAccess()
+    {
+        DefaultContent.Visibility = Visibility.Visible;
+        CrashContent.Visibility = Visibility.Collapsed;
+
+        // 此处应为检查程序完整性的代码，现在
+        // 临时使用延迟实现加载过程的等待效果
+        var random = new Random();
+        var next = random.Next(1000, 1500);
+
+        await Task.Delay(next);
+        return true;
+    }
+
+    private async Task OnNavigateToImpl()
+    {
+        var b = await CheckAccess();
+        if (b)
+        {
+            if (AppxContext.ServiceLocator.TryResolve<IWindowManagerService>(out var windowManagerService))
+            {
+                windowManagerService.NavigateTo(WindowFrameConstants.StartUp_Guid);
+            }
+            else
+            {
+                // 不应执行至此，但保留一段代码以显示错误
+                var guid = typeof(IWindowManagerService).GUID;
+
+                ShowCrashMessage(string.Format(_mrtCoreMap.GetString("Crash_Failed_To_Find_Component"), guid));
+                AppxContext.LoggingService.LogError($"AppxPreload: Failed to find component IID: {{{guid}}}.");
+            }
+        }
+        else
+        {
+            ShowCrashMessage(_mrtCoreMap.GetString("Crash_Failed_To_Check_Access"));
+            AppxContext.LoggingService.LogError($"AppxPreload: Failed to check access.");
+        }
     }
 
     protected override async void OnNavigatedTo(NavigationEventArgs e)
     {
-        var random = new Random();
-        var waitingTime = random.Next(1000, 2000);
-        Debug.WriteLine($"AppxPreload: Waiting Time = {waitingTime}");
-        await Task.Delay(waitingTime);
-
-        if (AppxContext.ServiceLocator.TryResolve<IWindowManagerService>(out var windowManagerService))
-        {
-            windowManagerService.NavigateTo(WindowFrameConstants.StartUp_Guid);
-        }
+        await OnNavigateToImpl();
     }
 }
