40656cb01e31e91637bc15de6fb41c50c90cf2a3
Wenveo <Wenveo@outlook.com>
2023-04-29 00:07:24 +0800

    在 GZSkinsX.Extensions.CreatorStudio 项目中添加导出导航项的定义以及添加默认页面

---
+++ src/extensions/CreatorStudio/GZSkinsX.Extensions.CreatorStudio/ExportNavigationItems.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 2022 - 2023 GZSkins, Inc. All rights reserved.
+// Licensed under the Mozilla Public License, Version 2.0 (the "License.txt").
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Composition;
+using System.Threading.Tasks;
+
+using GZSkinsX.Api.Controls;
+using GZSkinsX.Api.Navigation;
+
+using Windows.UI.Xaml.Controls;
+
+using Windows.UI.Xaml.Navigation;
+
+namespace GZSkinsX.Extensions.CreatorStudio;
+
+[Shared, ExportNavigationItem]
+[NavigationItemMetadata(Guid = NavigationConstants.DEV_TOOLS_CREATORSTUDIO_GUID, Header = "Creator Studio", PageType = typeof(ShellView),
+    Order = NavigationConstants.ORDER_DEV_TOOLS_GROUP_CREATORSTUDIO, OwnerGuid = NavigationConstants.DEV_TOOLS_GROUP)]
+internal sealed class ExportCreatorStudioNavigationItem : INavigationItem
+{
+    public IconElement Icon => new SegoeFluentIcon { Glyph = "\uEB3C" };
+
+    public async Task OnNavigatedFromAsync()
+    {
+        await Task.CompletedTask;
+    }
+
+    public async Task OnNavigatedToAsync(NavigationEventArgs args)
+    {
+        await Task.CompletedTask;
+    }
+}
