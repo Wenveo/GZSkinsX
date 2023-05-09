8a0269663b08c16ba979f4325046d8818343309c
Wenveo <Wenveo@outlook.com>
2023-05-09 22:44:30 +0800

    为 CommandItemGroupContext 和 CommandItemGroupContext 添加缺少的注释

--- src/appx/GZSkinsX/Commands/CommandItemContext.cs
+++ src/appx/GZSkinsX/Commands/CommandItemContext.cs
@@ -13,14 +13,29 @@ using GZSkinsX.Api.Commands;
 
 namespace GZSkinsX.Commands;
 
+/// <summary>
+/// 用于存储导出的 <see cref="ICommandItem"/> 对象以及 <see cref="CommandItemMetadataAttribute"/> 元数据
+/// </summary>
 internal sealed class CommandItemContext
 {
+    /// <summary>
+    /// 当前上下文中的懒加载对象
+    /// </summary>
     private readonly Lazy<ICommandItem, CommandItemMetadataAttribute> _lazy;
 
+    /// <summary>
+    /// 获取当前上下文的 <see cref="ICommandItem"/> 对象
+    /// </summary>
     public ICommandItem Value => _lazy.Value;
 
+    /// <summary>
+    /// 获取当前上下文的 <see cref="CommandItemMetadataAttribute"/> 元数据
+    /// </summary>
     public CommandItemMetadataAttribute Metadata => _lazy.Metadata;
 
+    /// <summary>
+    /// 初始化 <see cref="CommandItemContext"/> 的新实例
+    /// </summary>
     public CommandItemContext(Lazy<ICommandItem, CommandItemMetadataAttribute> lazy)
     {
         _lazy = lazy;
--- src/appx/GZSkinsX/Commands/CommandItemGroupContext.cs
+++ src/appx/GZSkinsX/Commands/CommandItemGroupContext.cs
@@ -11,14 +11,29 @@ using System.Collections.Generic;
 
 namespace GZSkinsX.Commands;
 
+/// <summary>
+/// 用于存储命令项的组的上下文信息
+/// </summary>
 internal sealed class CommandItemGroupContext
 {
+    /// <summary>
+    /// 获取该分组的名称
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// 获取该分组的排序顺序
+    /// </summary>
     public double Order { get; }
 
+    /// <summary>
+    /// 获取该分组中的子菜单项
+    /// </summary>
     public List<CommandItemContext> Items { get; }
 
+    /// <summary>
+    /// 初始化 <see cref="CommandItemGroupContext"/> 的新实例
+    /// </summary>
     public CommandItemGroupContext(string name, double order)
     {
         Name = name;
