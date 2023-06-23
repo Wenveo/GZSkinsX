0f888cb44a3807c6373e85917ac5afa94dd2e310
Wenveo <Wenveo@outlook.com>
2023-06-23 23:30:44 +0800

    添加 StartUp 页面和 WindowFrame 定义，以及相关的本地化资源

---
+++ src/appx/GZSkinsX.App/Views/WindowFrames/StartUp/StartUpFrame.cs
@@ -0,0 +1,46 @@
+// Copyright 2022 - 2023 GZSkins, Inc. All rights reserved.
+// Licensed under the Mozilla Public License, Version 2.0 (the "License.txt").
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Composition;
+
+using GZSkinsX.Api.Appx;
+using GZSkinsX.Api.Game;
+using GZSkinsX.Api.WindowManager;
+
+namespace GZSkinsX.Views.WindowFrames.StartUp;
+
+[Shared, ExportWindowFrame]
+[WindowFrameMetadata(Guid = WindowFrameConstants.StartUp_Guid, PageType = typeof(StartUpPage))]
+internal sealed class StartUpFrame : IWindowFrame
+{
+    private readonly IGameService _gameService;
+    private readonly IWindowManagerService _windowManagerService;
+
+    public StartUpFrame()
+    {
+        _gameService = AppxContext.GameService;
+        _windowManagerService = AppxContext.WindowManagerService;
+    }
+
+    public bool CanNavigateTo(WindowFrameNavigatingEvnetArgs args)
+    {
+        if (_gameService.TryUpdate(_gameService.RootDirectory, _gameService.CurrentRegion))
+        {
+            _windowManagerService.NavigateTo(WindowFrameConstants.NavigationRoot_Guid);
+            return false;
+        }
+        else
+        {
+            // IsInvalid
+            args.Parameter = _gameService.CurrentRegion != GameRegion.Unknown ||
+                string.IsNullOrEmpty(_gameService.RootDirectory) is false;
+
+            return true;
+        }
+    }
+}
+
---
+++ src/appx/GZSkinsX.App/Views/WindowFrames/StartUp/StartUpPage.xaml.cs
@@ -0,0 +1,110 @@
+// Copyright 2022 - 2023 GZSkins, Inc. All rights reserved.
+// Licensed under the Mozilla Public License, Version 2.0 (the "License.txt").
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System;
+
+using GZSkinsX.Api.Appx;
+using GZSkinsX.Api.Game;
+using GZSkinsX.Api.Helpers;
+using GZSkinsX.Api.WindowManager;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+
+using Windows.Storage.Pickers;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GZSkinsX.Views.WindowFrames.StartUp;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class StartUpPage : Page
+{
+    private readonly IWindowManagerService _windowManagerService;
+    private readonly IGameService _gameService;
+
+    public StartUpPage()
+    {
+        _windowManagerService = AppxContext.WindowManagerService;
+        _gameService = AppxContext.GameService;
+
+        InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        var isInvalid = e.Parameter is bool b && b;
+
+        // 设置标题
+        var localizedTitle = ResourceHelper.GetLocalized(isInvalid
+            ? "Resources/StartUp_Initialize_Invalid_Title"
+            : "Resources/StartUp_Initialize_Default_Title");
+        StartUp_Initialize_Title.Text = localizedTitle;
+
+        // 添加游戏区域枚举的本地化字符串至选择器列表
+        var riot = ResourceHelper.GetLocalized("Resources/StartUp_Initialize_Region_Riot");
+        StartUp_Initialize_Region_Selector.Items.Add(riot);
+
+        var tencent = ResourceHelper.GetLocalized("Resources/StartUp_Initialize_Region_Tencent");
+        StartUp_Initialize_Region_Selector.Items.Add(tencent);
+
+        var tooltip = ResourceHelper.GetLocalized("Resources/StartUp_Initialize_Region_Helper_ToolTip");
+        ToolTipService.SetToolTip(StartUp_Initialize_Region_Helper, tooltip);
+
+        base.OnNavigatedTo(e);
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+        StartUp_Initialize_Title.Text = string.Empty;
+        StartUp_Initialize_Region_Selector.Items.Clear();
+        base.OnNavigatedFrom(e);
+    }
+
+    private async void OnBrowser(object sender, RoutedEventArgs e)
+    {
+        var folder = await new FolderPicker().PickSingleFolderAsync();
+        if (folder is not null)
+        {
+            StartUp_Initialize_Directory_TextBox.Text = folder.Path;
+        }
+    }
+
+    private void ShowErrorMessage(string errorMsg)
+    {
+        StartUp_Initialize_Error_InfoBar.Title = errorMsg;
+        StartUp_Initialize_Error_InfoBar.IsOpen = true;
+    }
+
+    private void OnOK(object sender, RoutedEventArgs e)
+    {
+        var directoryPath = StartUp_Initialize_Directory_TextBox.Text;
+        if (string.IsNullOrEmpty(directoryPath))
+        {
+            ShowErrorMessage(ResourceHelper.GetLocalized("Resources/StartUp_Error_Directory_Null"));
+            return;
+        }
+
+        var selector = StartUp_Initialize_Region_Selector;
+        if (selector.SelectedIndex is -1)
+        {
+            ShowErrorMessage(ResourceHelper.GetLocalized("Resources/StartUp_Error_Region_Null"));
+            return;
+        }
+
+        if (!_gameService.TryUpdate(directoryPath, (GameRegion)(selector.SelectedIndex + 1)))
+        {
+            ShowErrorMessage(ResourceHelper.GetLocalized("Resources/StartUp_Error_Directory_Invalid"));
+            return;
+        }
+
+        _windowManagerService.NavigateTo(WindowFrameConstants.NavigationRoot_Guid);
+    }
+}
