0d4ceeb6a32e2b4f3ac9c9ab684af17d9a73a8bb
Wenveo <Wenveo@outlook.com>
2023-06-23 14:51:31 +0800

    添加 AutoThemeTitleBar.cs 随主题更改更新标题栏主题颜色的代码

---
+++ src/appx/GZSkinsX.App/Themes/AutoThemeTitleBar.cs
@@ -0,0 +1,53 @@
+// Copyright 2022 - 2023 GZSkins, Inc. All rights reserved.
+// Licensed under the Mozilla Public License, Version 2.0 (the "License.txt").
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Composition;
+
+using GZSkinsX.Api.Appx;
+using GZSkinsX.Api.Extension;
+using GZSkinsX.Api.Themes;
+
+using Microsoft.UI;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+
+namespace GZSkinsX.Themes;
+
+[Shared, ExportAdvanceExtension]
+[AdvanceExtensionMetadata(Trigger = AdvanceExtensionTrigger.AppLoaded)]
+internal sealed class AutoThemeTitleBar : IAdvanceExtension
+{
+    private readonly IAppxTitleBarButton _appxTitleBarButton;
+    private readonly IThemeService _themeService;
+
+    public AutoThemeTitleBar()
+    {
+        AppxContext.AppxWindow.MainWindow.SystemBackdrop = new MicaBackdrop();
+        _appxTitleBarButton = AppxContext.AppxTitleBarButton;
+
+        _themeService = AppxContext.ThemeService;
+        _themeService.ThemeChanged += OnThemeChanged;
+
+        DispatcherQueue.GetForCurrentThread().TryEnqueue(OnInitialize);
+    }
+
+    private void OnInitialize()
+    {
+        OnThemeChangedImpl(_themeService.ActualTheme);
+    }
+
+    private void OnThemeChanged(object? sender, ThemeChangedEventArgs e)
+    {
+        OnThemeChangedImpl(e.ActualTheme);
+    }
+
+    private void OnThemeChangedImpl(ElementTheme newTheme)
+    {
+        _appxTitleBarButton.ButtonForegroundColor = newTheme == ElementTheme.Light ? Colors.Black : Colors.White;
+    }
+}
