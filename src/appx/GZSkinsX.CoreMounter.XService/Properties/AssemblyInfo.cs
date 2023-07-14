89cb23f6c984a1f3a0c7c6fa9f8321c1eae4bcd7
Wenveo <Wenveo@outlook.com>
2023-07-14 23:35:14 +0800

    更新 GZSkinsX.CoreMounter.XService 中的 AssemblyInfo 信息

--- src/appx/GZSkinsX.CoreMounter.XService/Properties/AssemblyInfo.cs
+++ src/appx/GZSkinsX.CoreMounter.XService/Properties/AssemblyInfo.cs
@@ -1,15 +1,21 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+// Copyright 2022 - 2023 GZSkins, Inc. All rights reserved.
+// Licensed under the Mozilla Public License, Version 2.0 (the "License.txt").
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GZSkinsX.CoreMounter.XServices")]
+[assembly: AssemblyTitle("GZSkinsX.CoreMounter.XService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GZSkinsX.CoreMounter.XServices")]
+[assembly: AssemblyProduct("GZSkinsX.CoreMounter.XService")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
