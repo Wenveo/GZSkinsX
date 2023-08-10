8344ba1f7820c9290897b2171c7dfe6abcbefb90
Wenveo <Wenveo@outlook.com>
2023-08-10 11:01:45 +0800

    Add LocalizedString markup extension

---
+++ src/GZSkinsX.App/Markups/LocalizedStringExtension.cs
@@ -0,0 +1,39 @@
+// Copyright 2023 GZSkins, Inc. All rights reserved.
+// Licensed under the Mozilla Public License, Version 2.0 (the "LICENSE.txt").
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#nullable enable
+
+using GZSkinsX.Helpers;
+
+using Windows.UI.Xaml.Markup;
+
+namespace GZSkinsX.Markups;
+
+[MarkupExtensionReturnType(ReturnType = typeof(string))]
+internal sealed class LocalizedStringExtension : MarkupExtension
+{
+    public string? ResourceKey { get; set; }
+
+    protected override object ProvideValue()
+    {
+        if (string.IsNullOrEmpty(ResourceKey))
+        {
+            return string.Empty;
+        }
+
+        try
+        {
+#pragma warning disable CS8604
+            return ResourceHelper.GetLocalized(ResourceKey);
+#pragma warning restore CS8604
+        }
+        catch
+        {
+            return string.Empty;
+        }
+    }
+}
