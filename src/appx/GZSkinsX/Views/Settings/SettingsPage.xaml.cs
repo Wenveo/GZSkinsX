38683ef67a28bb70f69dab93697293ed00d49148
Wenveo <Wenveo@outlook.com>
2023-09-28 08:24:39 +0800

    Fix a bug of can't navigate to "Rate & Review"

--- src/appx/GZSkinsX/Views/Settings/SettingsPage.xaml.cs
+++ src/appx/GZSkinsX/Views/Settings/SettingsPage.xaml.cs
@@ -5,6 +5,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+using System;
+
 using CommunityToolkit.WinUI;
 
 using GZSkinsX.Contracts.Appx;
@@ -15,6 +17,8 @@ using GZSkinsX.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
+
+using Windows.System;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -40,5 +44,10 @@ internal sealed partial class SettingsPage : Page
     private void GoBack_Click(object sender, RoutedEventArgs e)
     {
         AppxContext.WindowManagerService.NavigateTo(WindowFrameConstants.Main_Guid);
+    }
+
+    private async void RateAndReview_Click(object sender, RoutedEventArgs e)
+    {
+        await Launcher.LaunchUriAsync(new("ms-windows-store://review/?PFN=61610AakStudio.64359D0915987_8fr87xx5s0whj"));
     }
 }
