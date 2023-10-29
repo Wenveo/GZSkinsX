454f76d9a9c8e065de52425b0833b3392a8f7ec5
Wenveo <Wenveo@outlook.com>
2023-10-29 23:13:11 +0800

    对 ExtensionMetadata 中的构造函数参数添加可空标记，并添加默认的空构造函数

--- src/appx/GZSkinsX.Appx.Contracts/Extension/ExtensionMetadata.cs
+++ src/appx/GZSkinsX.Appx.Contracts/Extension/ExtensionMetadata.cs
@@ -5,8 +5,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using System;
-
 using Microsoft.UI.Xaml.Controls;
 
 namespace GZSkinsX.Contracts.Extension;
@@ -19,7 +17,7 @@ public sealed record class ExtensionMetadata
     /// <summary>
     /// 获取扩展的唯一标识符。
     /// </summary>
-    public string? Id { get; }
+    public string? Id { get; init; }
 
     /// <summary>
     /// 获取显示在扩展管理器 UI 中的图标，如果没有指定图标元素，UI 将使用默认值。
@@ -29,17 +27,12 @@ public sealed record class ExtensionMetadata
     /// <summary>
     /// 获取扩展的版本。
     /// </summary>
-    public string? Version { get; }
+    public string? Version { get; init; }
 
     /// <summary>
     /// 获取扩展的发布者名称。
     /// </summary>
-    public string? PublisherName { get; }
-
-    /// <summary>
-    /// 获取显示在扩展管理器 UI 中的包及其内容的可选简短说明。
-    /// </summary>
-    public string? DisplayName { get; }
+    public string? PublisherName { get; init; }
 
     /// <summary>
     /// 获取显示在扩展管理器 UI 中的包及其内容的可选简短说明。
@@ -47,17 +40,32 @@ public sealed record class ExtensionMetadata
     public string? Desctiption { get; init; }
 
     /// <summary>
+    /// 获取显示在扩展管理器 UI 中的包及其内容的可选简短说明。
+    /// </summary>
+    public string? DisplayName { get; init; }
+
+    /// <summary>
+    /// 初始化 <see cref="ExtensionMetadata"/> 的新实例。
+    /// </summary>
+    public ExtensionMetadata()
+    {
+
+    }
+
+    /// <summary>
     /// 初始化 <see cref="ExtensionMetadata"/> 的新实例。
     /// </summary>
     /// <param name="id">扩展的唯一标识符。</param>
     /// <param name="version">扩展的版本。</param>
     /// <param name="publisherName">扩展的发布者名称。</param>
+    /// <param name="description">用于显示在扩展管理器 UI 中的描述说明。</param>
     /// <param name="displayName">用于显示在扩展管理器 UI 中的用户友好的包名称。</param>
-    public ExtensionMetadata(string id, string version, string publisherName, string displayName)
+    public ExtensionMetadata(string? id, string? version, string? publisherName, string? description, string? displayName)
     {
         Id = id;
         Version = version;
         PublisherName = publisherName;
+        Desctiption = description;
         DisplayName = displayName;
     }
 }
